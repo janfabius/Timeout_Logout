8b13104099e71773072b14e1df64d9e1ec71bcf6
janfabius <adanet18@gmail.com>
2022-11-03 17:34:39 +0100

    upg

--- app/src/main/java/com/example/timeout_logout/MainActivity.java
+++ app/src/main/java/com/example/timeout_logout/MainActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 
 import android.util.Log;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
@@ -14,7 +15,12 @@ import java.util.TimerTask;
 public class MainActivity extends AppCompatActivity {
 
     private Timer timer;
+    private Timer timer2;
     private Toolbar toolbar;
+
+    TextView t_inter;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,28 +28,65 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
        // toolbar = (Toolbar) findViewById(R.id.toolbar);
         //setSupportActionBar(toolbar);
+
+
+        t_inter = findViewById(R.id.txt_inter);
+
+
+
     }
 
     @Override
     protected void onPause() {
         super.onPause();
 
-        timer = new Timer();
-        Log.i("Main", "Invoking logout timer");
-        LogOutTimerTask logoutTimeTask = new LogOutTimerTask();
-        //timer.schedule(logoutTimeTask, 5*60*1000); //auto logout in 5 minutes
-
-        timer.schedule(logoutTimeTask, 1*60*1000); // 1 min
+//        timer2 = new Timer();
+//        Log.i("Main", "Invoking logout timer");
+//        LogOutTimerTask logoutTimeTask = new LogOutTimerTask();
+//        //timer.schedule(logoutTimeTask, 5*60*1000); //auto logout in 5 minutes
+//
+//        timer2.schedule(logoutTimeTask, 5000); // 5 sec
     }
 
     @Override
-    protected void onResume() {
-        super.onResume();
+    public void onUserInteraction() {
+        super.onUserInteraction();
+
+//        if (timer2 != null) {
+//            timer2.cancel();
+//            timer2 = null;
+//        }
+
+        t_inter.setText("Stai interagendo!");
+
         if (timer != null) {
             timer.cancel();
             Log.i("Main", "cancel timer");
             timer = null;
         }
+
+        timer = new Timer();
+        Log.i("Main", "Invoking logout timer");
+        LogOutTimerTask logoutTimeTask = new LogOutTimerTask();
+        timer.schedule(logoutTimeTask, 5000); // 5 sec
+
+    }
+
+
+
+
+
+
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+//        if (timer2 != null) {
+//            timer2.cancel();
+//            Log.i("Main", "cancel timer");
+//            timer2 = null;
+//        }
+
     }
 
     private class LogOutTimerTask extends TimerTask {
@@ -55,7 +98,8 @@ public class MainActivity extends AppCompatActivity {
             Intent i = new Intent(MainActivity.this, LoginActivity.class);
             i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
             startActivity(i);
-            finish();
+            //finish();
+            System.exit(0);
         }
     }
 }
